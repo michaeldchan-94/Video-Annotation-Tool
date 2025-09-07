acce449f3b1dce1b528887998354f1336ac2ad57
Michael Chan <michaeldchan94@gmail.com>
2025-09-07 15:18:41 -0400

    Working Validate Annotations

--- Validate_Annotation.py
+++ Validate_Annotation.py
@@ -11,9 +11,7 @@ import sys
 from typing import Any, List
 from numpy import dtype, floating, integer, ndarray
 import cv2
-
-
-from utils import apply_infobar, read_annotations,Annotation
+from utils import apply_infobar, get_optimal_font_scale, read_annotations,Annotation
 
 
 class AnnotationValidator:
@@ -43,6 +41,7 @@ class AnnotationValidator:
         while cap.isOpened():
             # Only get the next frame if appropriate key bindings have been pressed
             if (self.get_next_frame):
+                cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_number)
                 ret, frame = cap.read()   
                 if not ret:
                     print("End of video.")
@@ -59,60 +58,53 @@ class AnnotationValidator:
                 
                 apply_infobar(self.cur_frame ,self.normal_keyboard_options,self.frame_number,self.width)
 
-                cv2.imshow('Video Stream', self.cur_frame)
+            cv2.imshow('Video Stream', self.cur_frame)
 
-                self.get_next_frame = False
-                key = cv2.waitKey(0) & 0xFF
-                if key == ord('q'):
-                    break
-                elif key == ord('n'):
-                    self.onNextFrame()
-                    self.get_next_frame = True
-                    self.frame_number += 1
-                elif key == ord('N'):
-                    self.onForward10()
-                    self.get_next_frame = True
-                    self.frame_number += 1
-                elif key == ord('p'):
-                    self.onPreviousFrame()
-                    self.get_next_frame = True
-                    self.frame_number += 1
-                elif key == ord('P'):
-                    self.onBack10()
-                    self.get_next_frame = True
-                    self.frame_number += 1
-    
-    def onNextFrame(self):
-        pass
-    def onForward10(self):
-        pass
-    def onPreviousFrame(self):
-        pass
-    def onBack10(self):
-        pass
+            self.get_next_frame = False
+            key = cv2.waitKey(0) & 0xFF
+            if key == ord('q'):
+                break
+            elif key == ord('n'):
+                self.frame_number += 1
+                self.get_next_frame = True
+            elif key == ord('N'):
+                self.frame_number += 10
+                self.get_next_frame = True
+            elif key == ord('p'):
+                self.frame_number = max(0, self.frame_number - 1)
+                self.get_next_frame = True
+            elif key == ord('P'):
+                self.frame_number = max(0, self.frame_number - 10)
+                self.get_next_frame = True
 
     def __apply_annotation(self, frame : cv2.Mat | ndarray[Any, dtype[integer[Any] | floating[Any]]],
                            annotation : Annotation):
-        bottom_x : int = annotation.center_x-int(annotation.width/2)
-        bottom_y : int = annotation.center_y-int(annotation.height/2)
-        upper_x : int = annotation.center_x+int(annotation.width/2)
-        upper_y : int = annotation.center_y+int(annotation.height/2)
-        frame = cv2.rectangle(frame, (bottom_x,bottom_y),(upper_x,upper_y), (0, 255, 0), 2)
+        if (annotation.annotation_type == "V"):
+            bottom_x : int = annotation.center_x-int(annotation.width/2)
+            bottom_y : int = annotation.center_y-int(annotation.height/2)
+            upper_x : int = annotation.center_x+int(annotation.width/2)
+            upper_y : int = annotation.center_y+int(annotation.height/2)
+            frame = cv2.rectangle(frame, (bottom_x,bottom_y),(upper_x,upper_y), (0, 255, 0), 2)
+        else:
+            text : str = f'Annotation is {annotation.annotation_type} for Frame : {self.frame_number}'
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = get_optimal_font_scale(text,self.width)
+            color = (0, 0, 255) # White color (B, G, R)
+            thickness = 2
+            org = (0, int(self.height * 0.95)) # (x, y) coordinates for bottom-left corner of the text
+            return cv2.putText(frame, text, org, font, font_scale, color, thickness, cv2.LINE_AA)
 
 
 
 
 if __name__ == "__main__":
     annotation_validator = AnnotationValidator()
-    # arguments = sys.argv
-    # if len(arguments) < 2:
-    #     print("Please specify a video file to annotate!")
-    #     sys.exit(1)
-    # video_path = arguments[1]
-    # annotation_output_path = ""
-    # if len(arguments) == 3:
-    #     annotation_output_path = arguments[2]
-
-    video_path = "test/853889-hd_1920_1080_25fps.mp4"
-    annotation_output_path = "test/853889-hd_1920_1080_25fps.annotations"
+    arguments = sys.argv
+    if len(arguments) < 2:
+        print("Please specify a video file to annotate!")
+        sys.exit(1)
+    video_path = arguments[1]
+    annotation_output_path = ""
+    if len(arguments) == 3:
+        annotation_output_path = arguments[2]
     annotation_validator.ReadAnnotations(video_path,annotation_output_path)
